68b203d6f9b3b1d8bd2fe635fe48a22ee87f6be2
Renju Radhakrishnan <renju467@gmail.com>
2017-02-12 12:47:51 -0500

    commented json print, total count

--- src/main/java/phd/Main.java
+++ src/main/java/phd/Main.java
@@ -12,39 +12,41 @@ public class Main {
 		
 	    ArrayList<Users> testUsers = new ArrayList<>();
 		int counter = 0;
+		int errorCount = 0;
 		for(Users user : users) {
-			counter++;
-			if(counter > 25) {
-				break;
-			}
 			System.out.println("-----------------------------");
 			System.out.println(user.getFirst_name() + " " + user.getLast_name());
 			System.out.println("Field of interest: " + user.getField_of_interest());
 			try {
 				Users crawledUserData = crawler.getWebPage(user.getSource1());
 				if(crawledUserData.getEmail().size() == 0 || crawledUserData.getEmail() == null) {
-					break;
-				}
-				
-				if(user.getFirst_name().equals("Jieping")) {
-					System.out.println("BREAK HERE");
-					break;
+					continue;
 				}
 				user.setBio(crawledUserData.getBio());
 				user.setNumber(crawledUserData.getNumber());
 				user.setEmail(crawledUserData.getEmail());
 				user.setImages(crawledUserData.getImages());
 			} catch(Exception e) {
-				
+				errorCount++;
 			}
+			counter++;
 			testUsers.add(user);
+			
 			
 		}
 		
 		String jsonArray = JsonParser.toJson(testUsers);
-		System.out.println(jsonArray);
+		//System.out.println(jsonArray);
+
 		PrintWriter out = new PrintWriter("csProfData.json");
+		PrintWriter stats = new PrintWriter("crawler.log");
+
+		stats.println("Total number of professors crawled: " + counter);
+		stats.println("Total number of errors: " + errorCount);
+
 		out.println(jsonArray);
+		
+		stats.close();
 		out.close();
 	}
 }
